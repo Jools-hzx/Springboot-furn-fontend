59c27d3bb1782c5ef5ec47aa67a72b77116c2e92
Jools <1035558517@qq.com>
2023-05-09 18:27:15 +0800

    Build template index page for Vue project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+<!--      引用组件-->
+      <Header/>
+<!--      主体-->
+      <div style="display: flex">
+<!--          加入侧边栏-->
+          <Aside/>
+<!--          内容区域： 这个部分是从 HomeView.vue 组件来的(通过路由机制)-->
+          <router-view style="flex: 1"/>
+      </div>
+  </div>
 </template>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+
+<script>
+import Header from "@/components/Header.vue";
+import Aside from "@/components/Aside.vue";
+export default  {
+    name: "Layout",
+    components : {
+        Header,
+        Aside
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//  引入css
+import '@/assets/css/global.css'
 
-createApp(App).use(store).use(router).mount('#app')
+// 引入 Element-Plus
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+
+//处理国际化
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+createApp(App).use(store).use(router).use(ElementPlus, {
+    locale: zhCn,
+}).mount('#app')
